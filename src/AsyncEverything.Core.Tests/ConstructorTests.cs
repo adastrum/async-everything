239b29e674d30469e679766ec878c5ced4ac72ad
Dmitry Trofimov <trofimovde@outlook.com>
2019-09-10 20:58:30 +0200

    Add async factory and initialization example

---
+++ src/AsyncEverything.Core.Tests/ConstructorTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace AsyncEverything.Core.Tests
+{
+    public class ConstructorTests
+    {
+        [Fact]
+        public async Task FactoryMethod()
+        {
+            var bar = await Bar.CreateAsync();
+        }
+
+        [Fact]
+        public async Task AsyncInitialization()
+        {
+            //var baz = new Baz();
+            var baz = Activator.CreateInstance<Baz>();
+
+            await baz.Initialization;
+        }
+    }
+}
---
+++ src/AsyncEverything.Core/Constructor.cs
@@ -0,0 +1,48 @@
+﻿using System.Threading.Tasks;
+
+namespace AsyncEverything.Core
+{
+    public class Bar
+    {
+        private Bar()
+        {
+
+        }
+
+        private async Task<Bar> InitializeAsync()
+        {
+            await Task.Yield();
+
+            return this;
+        }
+
+        public static async Task<Bar> CreateAsync()
+        {
+            var result = new Bar();
+
+            await result.InitializeAsync();
+
+            return result;
+        }
+    }
+
+    public interface IAsyncInitialization
+    {
+        Task Initialization { get; }
+    }
+
+    public class Baz : IAsyncInitialization
+    {
+        public Baz()
+        {
+            Initialization = InitializeAsync();
+        }
+
+        public Task Initialization { get; }
+
+        private async Task InitializeAsync()
+        {
+            await Task.Yield();
+        }
+    }
+}
