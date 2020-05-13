9dcc2b91cf97c28fbe5196f445ad89f7bcd97cd4
Dmitry Trofimov <trofimovde@outlook.com>
2020-05-13 20:58:02 +0200

    Add async console

---
+++ src/AsyncEverything.Core/AsyncConsole.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+
+namespace AsyncEverything.Core
+{
+    public static class AsyncConsole
+    {
+        const int STD_INPUT_HANDLE = -10;
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        private static extern IntPtr GetStdHandle(int nStdHandle);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        private static extern bool CancelIoEx(IntPtr handle, IntPtr lpOverlapped);
+
+        public static Task<string> ReadLineAsync(TimeSpan timeout)
+        {
+            string result = null;
+
+            return Task.WhenAny(
+                Task.Run(async () =>
+                {
+                    await Task.Delay(timeout);
+
+                    if (string.IsNullOrEmpty(result))
+                    {
+                        var handle = GetStdHandle(STD_INPUT_HANDLE);
+                        CancelIoEx(handle, IntPtr.Zero);
+
+                        return "Timeout";
+                    }
+
+                    return result;
+                }),
+                Task.Run(Console.ReadLine)).Unwrap();
+        }
+
+        public static Task WriteLineAsync(string value)
+        {
+            return Task.Run(() => Console.WriteLine(value));
+        }
+    }
+}
